4ff790e7a1a0ec64067945528d4e4698344d66b3
Mykhailo <dzhammykhailo@knu.ua>
2025-05-10 23:55:18 +0300

    Міша ## Фігури: Додав усі фігури

--- Chess/classes/Figures.py
+++ Chess/classes/Figures.py
@@ -7,3 +7,20 @@ class Rook(Figure):
 class Bishop(Figure):
     def __init__(self, color):
         super().__init__(color)
+
+class King(Figure):
+    def __init__(self, color):
+        super().__init__(color)
+
+class Queen(Figure):
+    def __init__(self, color):
+        super().__init__(color)
+
+class Knight(Figure):
+    def __init__(self, color):
+        super().__init__(color)
+
+class Pawn(Figure):
+    def __init__(self, color):
+        super().__init__(color)
+
